75b4ec61e5b756601fbc145be3ed2f1ee46b7b52
Tanayakharabe <tanayakharabe00@gmail.com>
2026-06-26 11:56:13 +0530

    Add files via upload

---
+++ arraylistdemo/src/arraylistdemo/Arraylist.java
@@ -0,0 +1,34 @@
+package arraylistdemo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 ArrayList<String> Student = new ArrayList<String>();
+		 Student.add("Amit");
+		 Student.add("Arbaz");
+		 Student.add("Mitali");
+		 System.out.println(Student);
+		 
+		 //different prog 
+		 LinkedList<Integer> rollNo= new LinkedList<Integer>();
+		 rollNo.add(10);
+		 rollNo.add(20);
+		 rollNo.add(30);
+		 System.out.println(rollNo);
+		 
+		 
+		 
+		 Vector<String> names = new Vector<String>();
+		 
+		 
+
+	}
+
+}
+
+

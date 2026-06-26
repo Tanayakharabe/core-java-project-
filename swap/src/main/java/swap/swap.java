1eb4878edb61e6e7b44e8ac74a272f42e28a1499
Tanayakharabe <tanayakharabe00@gmail.com>
2026-06-26 12:42:46 +0530

    Add files via upload

---
+++ swap/src/main/java/swap/swap.java
@@ -0,0 +1,23 @@
+package swap;
+
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			int x=100,y=200;
+			System.out.println("before swap");
+			System.out.println("x="+x);
+			System.out.println("y="+y);
+			
+			int temp = x;
+			x = y;
+			y =temp;
+			
+			System.out.println("after swap");
+			System.out.println("x="+x);
+			System.out.println("y="+y);
+		}
+
+}
+}
